f569a6996608ae59b2671cfcd2e9fdd786e1ce46
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-27 01:10:12 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -36,7 +36,10 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+aoh[2][[:name] = "Pat Monteseian"
+
 end
+
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
